90961bf725d3b3396e39822c9ab60fc37ef5efb0
Kushwaha Vikash <vikas.kushwaha@benchwork.in>
2021-06-19 23:10:06 +0530

    Routes

--- server.js
+++ server.js
@@ -10,5 +10,11 @@ app.get('/',(req,res)=>{
     res.send('Hello world');
 })
 
+// Define Routes
+app.use('/api/auth',require('./routes/api/Auth'))
+app.use('/api/users',require('./routes/api/User'))
+app.use('/api/posts',require('./routes/api/Posts'))
+app.use('/api/profile',require('./routes/api/Profile'))
+
 const PORT = process.env.PORT || 5000; 
 app.listen(PORT, ()=>{console.log(`Server started on port ${PORT}`)})
